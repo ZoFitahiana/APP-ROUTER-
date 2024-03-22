4eb4b42bc82bf66dafbeaaa9c6fda4909dd5af58
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-22 18:30:41 +0100

    feat : Create page detail of pokemon client

---
+++ src/app/detail/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DetailOfPokemon from "../../../../component/detail/detail";
+
+export default function detailOfPokemon(){
+    return <>
+    <DetailOfPokemon/>
+    </>
+}
