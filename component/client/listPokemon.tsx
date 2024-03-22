a8ce2c891c0fb13ef89929adf3e0bb6a4ade300b
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-22 16:25:31 +0100

    fix : Update method for client to receive image of pokemon

--- component/client/listPokemon.tsx
+++ component/client/listPokemon.tsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useEffect, useState } from "react";
 
-
 interface Pokemon {
     name: string,
     url: string
@@ -44,43 +43,37 @@ export default function PokemonList() {
     };
 
     return (
-        <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Account ID
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(pok => (
-                        <tr key={pok.name} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td className="px-6 py-4">
-                                name :
-                                <br />
-                                <br />
-                                {pok.name}
-                                <br />
-                                <br />
-                                <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Détails</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="flex flex-wrap justify-center gap-6">
+            {data.map(pokemon => (
+                <div key={pokemon.name} className="card w-96 bg-base-100 shadow-xl image-full">
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIdFromUrl(pokemon.url)}.png`} alt={pokemon.name} />
+                    <div className="card-body">
+                        <h2 className="card-title">{pokemon.name}</h2>
+                        <p>Pokemon number :  {getIdFromUrl(pokemon.url)}  🍳</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">
+                                <a href={`/detail/${getIdFromUrl(pokemon.url)}`}>Details</a></button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            
             <div className="mt-4 flex justify-between">
                 <button onClick={prevPage} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
                     </svg>
                 </button>
                 <button onClick={nextPage} className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
                     </svg>
                 </button>
             </div>
         </div>
     )
+}
+
+function getIdFromUrl(url: string): string {
+    return url.split("/").slice(-2, -1)[0];
 }
